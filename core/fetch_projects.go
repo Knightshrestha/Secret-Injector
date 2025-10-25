8ec1453834ee11f7401fa6002a3aa46e89273f04
Knight Shrestha <discrilkx379@gmail.com>
2025-10-25 07:55:07 +0545

    move to use read only database

--- core/fetch_projects.go
+++ core/fetch_projects.go
@@ -2,34 +2,25 @@ package core
 
 import (
 	"context"
-	"io"
-	"log"
-	"os"
 
 	"github.com/Knightshrestha/Secret-Injector/database"
 	"github.com/Knightshrestha/Secret-Injector/database/generated"
 )
 
-func FetchProjects() []generated.ProjectList {
-	log.SetOutput(io.Discard)
-	mainDb := database.OpenDatabase()
-	log.SetOutput(os.Stdout)
-
-	allProjects, err := mainDb.ReadQueries.GetAllProjects(context.Background())
+func FetchProjects() ([]generated.ProjectList, error) {
+	mainDb, err := database.OpenReadDatabase()
 	if err != nil {
-		log.Fatalf("Could not fetch project list: %s", err)
-
+		return nil, err
 	}
 
-	if mainDb.WriteDB != nil || mainDb.ReadDB != nil {
-		// log.Println("Closing database connections...")
-		if err := database.CloseDatabase(mainDb); err != nil {
-			log.Println("Error closing database:", err)
-		} else {
-			// log.Println("✓ Database connections closed successfully")
-		}
+	allProjects, err := mainDb.Queries.GetAllProjects(context.Background())
+	if err != nil {
+		return nil, err
 	}
 
-	return allProjects
+	if err := database.CloseReadDatabase(mainDb.DB); err != nil {
+		return nil, err
+	}
 
+	return allProjects, nil
 }
